f24a52c07bfea68818d86fafa4b0ad035441462e
Mason <mhancock5466@gmail.com>
2018-09-25 10:31:17 -0500

    Update

--- Computer Guess/Computer Guess/Source.cpp
+++ Computer Guess/Computer Guess/Source.cpp
@@ -13,32 +13,44 @@ using namespace std::chrono;
 using namespace std::this_thread;
 
 int main() {
-	int userNumber;
-	int computerNumber = 0;
-	int upperLimit = 100;
-	int lowerLimit = 0;
-	int attempts = 0;
+	int replay = 1;
 
-	cout << "Choose a number between " << lowerLimit + 1 << " and " << upperLimit << "." << endl;
-	cout << "The computer will attempt to guess your number.\nYour Choice: ";
-	cin >> userNumber;
+	do {
+		int userNumber;
+		int computerNumber = 0;
+		int upperLimit = 100;
+		int lowerLimit = 0;
+		int attempts = 0;
+	
+		cout << "Choose a number between " << lowerLimit + 1 << " and " << upperLimit << "." << endl;
+		cout << "The computer will attempt to guess your number.\nYour Choice: ";
+		cin >> userNumber;
 
-	while (computerNumber != userNumber) {
-		srand(time(NULL));
-		computerNumber = rand() % (upperLimit - lowerLimit) + (lowerLimit + 1);
+		while (userNumber > upperLimit || userNumber <= lowerLimit) {
+			cout << "\nUser did not input a valid number. Please input a number between " << lowerLimit + 1 << " and " << upperLimit << ". ";
+			cin >> userNumber;
+		}
 
-		attempts++;
-		cout << "\nComputer Guess: " << computerNumber << "\nComputer Attempts: " << attempts << endl;
+		while (computerNumber != userNumber) {
+			srand(time(NULL));
+			computerNumber = rand() % (upperLimit - lowerLimit) + (lowerLimit + 1);
+
+			attempts++;
+			cout << "\nComputer Guess: " << computerNumber << "\nComputer Attempts: " << attempts << endl;
+			cout << endl;
+			if (computerNumber > userNumber)
+				upperLimit = computerNumber;
+			else if (computerNumber < userNumber)
+				lowerLimit = computerNumber;
+			sleep_for(seconds(1));
+		}
+
+		cout << "The computer solved your random number!" << "\nIt only took it " << attempts << " tries." << endl;
+		cout << "\nWould you like to play again?\n1 - Yes\n2 - No\nYour Choice: ";
+		cin >> replay;
 		cout << endl;
-		if (computerNumber > userNumber)
-			upperLimit = computerNumber;
-		else if (computerNumber < userNumber)
-			lowerLimit = computerNumber;
-		sleep_for(seconds(1));
-	}
 
-	cout << "The computer solved your random number!" << "\nIt only took it " << attempts << " tries." << endl;
-	cout << endl;
+	} while (replay == 1);
 
 	system("pause");
 	return 0;
